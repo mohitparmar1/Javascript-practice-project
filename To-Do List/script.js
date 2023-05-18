3f993e7aa892550603be5b62e6137df351aefaa3
mohitparmar1 <contact.mohit06@gmail.com>
2023-05-18 16:05:15 +0530

    added some files

---
+++ To-Do List/script.js
@@ -0,0 +1,13 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if(inputBox.value === ""){
+        alert("Please enter a task");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+};
