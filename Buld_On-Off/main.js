70f7d29c1cb9b86c4e70681b99607f7e6748694d
mohitparmar1 <contact.mohit06@gmail.com>
2023-05-21 16:07:10 +0530

    changed button color

--- Buld_On-Off/main.js
+++ Buld_On-Off/main.js
@@ -1,12 +1,14 @@
 const bulbEl = document.getElementById("bulb");
-const bulbOn = document.getElementById("bulb-on");
+const bulbSwitch = document.getElementById("bulb-switch");
 
 
-bulbOn.addEventListener("click", () => {
+bulbSwitch.addEventListener("click", () => {
     if(bulbEl.src.match("off")) {
         bulbEl.src = "./bulb-on.gif";
+        bulbSwitch.innerText = "Turn Off";
     }else{
         bulbEl.src = "./bulb-off.gif";
+        bulbSwitch.innerText = "Turn On";
     }
  
 });
